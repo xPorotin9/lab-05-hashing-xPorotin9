7c1de80bbf0046c1b9e14d36fbd84ff782cb9efe
Jose Carlos Machaca Vera <130808338+xPorotin9@users.noreply.github.com>
2023-10-23 21:04:31 -0600

    Creación de archivo py del codigo inicial

---
+++ codes/Hashtable.py
@@ -0,0 +1,54 @@
+import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
+from random import choice
+%matplotlib inline
+class HashTable:
+    ## code
+    ## code
+    ## def insert(self, x)
+    ## def delete(self, x)
+    ## def find(self, x)
+def mi_Mod(x,n=10):
+    ## CODE
+print(mi_Mod(52))
+print(mi_Mod(3235235))
+HT = HashTable(mi_Modn, 10)
+
+x = 1234567
+y = 76554334234
+HT.insert(x)
+
+## verificar que la tabla hash funciona
+def randomFn(x, n=10):
+    ##code
+print(randomFn(52))
+print(randomFn(3235235))
+HT = HashTable(randomFn, 10)
+
+x = 1234567
+y = 76554334234
+HT.insert(x)
+print(randomFn(52))
+print(randomFn(3235235))
+print(randomFn(52))
+print(randomFn(3235235))
+def RandomHashFun(M, n=10):
+    fnTable = [ None for i in range(M) ]
+    for x in range(M):
+        fnTable[x] = choice(range(n))
+    def randomFn(x):
+        return fnTable[x]
+    return randomFn
+randomFn2 = RandomHashFun(1000, 10)
+print(randomFn2(52))
+print(randomFn2(324))
+print(randomFn2(52))
+print(randomFn2(324))
+HT = HashTable(randomFnTake2, 10)
+
+x = 123
+y = 76
+HT.insert(x)
+
+## verificar que la tabla hash funciona
